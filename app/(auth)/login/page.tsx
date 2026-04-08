1585ad89cbd5bd7dce751e467e974471f649322c
Mohamed Reda Ali <m07hamedreda25@gmail.com>
2026-04-08 23:48:28 +0200

    Initial

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LuGithub } from "react-icons/lu";
 
 const LogIn = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-xl">Welcome back!</CardTitle>
+          <CardDescription>Login with your Github Email Accout</CardDescription>
+        </CardHeader>
 
-export default LogIn
+        <CardContent>
+          <Button className="w-full" variant={"outline"}>
+            <LuGithub />
+            Sign in with Github
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default LogIn;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-red-500">Hello</h1>
+      <ThemeToggle />
     </div>
   );
 }
