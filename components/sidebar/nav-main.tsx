387b47acb216a02330f191f813b7c7ca289bf677
Mohamed Reda Ali <m07hamedreda25@gmail.com>
2026-04-10 23:26:43 +0200

    Finishing admin dash

--- components/sidebar/nav-main.tsx
+++ components/sidebar/nav-main.tsx
@@ -25,31 +25,24 @@ export function NavMain({
         <SidebarMenu>
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
-              asChild
+              render={<Link href="/admin/course/create" />}
               tooltip="Quick Create"
               className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
             >
-              <Link
-                href={"/admin/course/create"}
-                className="flex gap-1.5 items-center justify-center"
-              >
-                <CirclePlusIcon />
-                <span>Quick Create</span>
-              </Link>
+              <CirclePlusIcon />
+              <span>Quick Create</span>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton tooltip={item.title} asChild>
-                <Link
-                  href={item.url}
-                  className="flex gap-1.5 items-center justify-center"
-                >
-                  {item.icon}
-                  <span>{item.title}</span>
-                </Link>
+              <SidebarMenuButton
+                tooltip={item.title}
+                render={<Link href={item.url} />}
+              >
+                {item.icon}
+                <span>{item.title}</span>
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
