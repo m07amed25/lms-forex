27287f6070aa433f958ce35932e40311f79a8fb2
Mohamed Reda Ali <m07hamedreda25@gmail.com>
2026-04-10 22:00:59 +0200

    Admin

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import * as React from "react";
+
+import { NavMain } from "@/components/nav-main";
+import { NavSecondary } from "@/components/nav-secondary";
+import { NavUser } from "@/components/nav-user";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  LayoutDashboardIcon,
+  ListIcon,
+  ChartBarIcon,
+  FolderIcon,
+  UsersIcon,
+  CameraIcon,
+  FileTextIcon,
+  Settings2Icon,
+  CircleHelpIcon,
+  SearchIcon,
+} from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+
+const data = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/admin",
+      icon: <LayoutDashboardIcon />,
+    },
+    {
+      title: "Courses",
+      url: "/admin/courses",
+      icon: <ListIcon />,
+    },
+    {
+      title: "Analytics",
+      url: "#",
+      icon: <ChartBarIcon />,
+    },
+    {
+      title: "Projects",
+      url: "#",
+      icon: <FolderIcon />,
+    },
+    {
+      title: "Team",
+      url: "#",
+      icon: <UsersIcon />,
+    },
+  ],
+  navClouds: [
+    {
+      title: "Capture",
+      icon: <CameraIcon />,
+      isActive: true,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Proposal",
+      icon: <FileTextIcon />,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Prompts",
+      icon: <FileTextIcon />,
+      url: "#",
+      items: [
+        {
+          title: "Active Proposals",
+          url: "#",
+        },
+        {
+          title: "Archived",
+          url: "#",
+        },
+      ],
+    },
+  ],
+  navSecondary: [
+    {
+      title: "Settings",
+      url: "#",
+      icon: <Settings2Icon />,
+    },
+    {
+      title: "Get Help",
+      url: "#",
+      icon: <CircleHelpIcon />,
+    },
+    {
+      title: "Search",
+      url: "#",
+      icon: <SearchIcon />,
+    },
+  ],
+};
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="offcanvas" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton className="data-[slot=sidebar-menu-button]:p-1.5!">
+              <Link className="flex items-center gap-2.5" href="/">
+                <div className="flex size-9 items-center justify-center">
+                  <Image
+                    src="/logo.png"
+                    alt="logo"
+                    width={24}
+                    height={24}
+                    className="object-contain"
+                  />
+                </div>
+                <span className="text-xl font-bold tracking-tight text-foreground/90">
+                  Forex<span className="text-foreground">With</span>
+                  <span className="mx-[2px] text-primary">.</span>
+                  <span className="bg-linear-to-tr from-primary to-primary/60 bg-clip-text font-extrabold text-transparent">
+                    Salma
+                  </span>
+                </span>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        <NavSecondary items={data.navSecondary} className="mt-auto" />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
